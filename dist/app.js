07fe1142cbfc9c702ec8ef400bbf9456fe489cbf
Анастасия Гущина <ukin7584@gmail.com>
2022-02-06 17:55:38 +0300

    added ngrok

--- dist/app.js
+++ dist/app.js
@@ -1,23 +1,4 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -27,33 +8,65 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const net = __importStar(require("net"));
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 const PORT = 3000;
-const IP = '127.0.0.1';
+const IP = "127.0.0.1";
 const BACKLOG = 100;
 const config = {
     host: "localhost",
     user: "root",
     database: "catalina",
-    password: "123"
+    password: "123",
 };
-net.createServer()
+const divideStringOn = (s, search) => {
+    const index = s.indexOf(search);
+    const first = s.slice(0, index);
+    const rest = s.slice(index + search.length);
+    return [first, rest];
+};
+const parseRequest = (s) => {
+    const [firstLine, rest] = divideStringOn(s, "\r\n");
+    const [method, url, protocol] = firstLine.split(" ", 3);
+    const [headers, body] = divideStringOn(rest, "\r\n\r\n");
+    const parsedHeaders = headers.split("\r\n").reduce((map, header) => {
+        const [key, value] = divideStringOn(header, ": ");
+        return map.set(key, value);
+    }, new Map());
+    return { protocol, method, url, headers: parsedHeaders, body };
+};
+const compileResponse = (r) => `${r.protocol} ${r.statusCode} ${r.status}
+  ${Array.from(r.headers)
+    .map((kv) => `${kv[0]}: 
+  ${kv[1]}`)
+    .join("\r\n")}
+  ${r.body}`;
+// /
+// main function start
+net
+    .createServer()
     .listen(PORT, IP, BACKLOG)
-    .on('connection', socket => {
-    socket
-        .on('data', buffer => {
+    .on("connection", (socket) => {
+    // socket.allowHalfOpen=true;
+    socket.on("data", (buffer) => {
         const request = parseRequest(buffer.toString());
         console.log(request);
-        f(request.body, request.url).then(res => {
+        f(request.body, request.url).then((res) => {
             console.log(res);
             socket.write(compileResponse({
-                protocol: 'HTTP/1.1',
+                protocol: "HTTP/1.1",
                 headers: new Map(),
-                status: 'OK',
+                status: "OK",
                 statusCode: 200,
-                body: res
+                body: res,
             }));
             socket.end();
         });
@@ -69,44 +82,28 @@ function get(query) {
 }
 function f(body, url) {
     return __awaiter(this, void 0, void 0, function* () {
-        var a = "";
+        let a = "";
         if (url.includes("/?login=")) {
             try {
-                var t = JSON.parse(body);
-                yield get(`SELECT name,role FROM catalina.user WHERE login='${t.login}' AND password='${t.password}'`).then(res => { a = res; });
+                const t = JSON.parse(body);
+                yield get(`SELECT name,role FROM catalina.user WHERE login='${t.login}' AND password='${t.password}'`).then((res) => {
+                    a = res;
+                });
             }
             catch (err) {
                 console.log("JSON is invalid");
             }
-            ;
         }
         else if (url.includes("/?salons")) {
-            yield get(`SELECT coordinate_x, coordinate_y, address, caption FROM catalina.salon`).then(res => { a = res; });
+            yield get("SELECT coordinate_x, coordinate_y, address, caption FROM catalina.salon").then((res) => {
+                a = res;
+            });
         }
-        else { }
+        else {
+            // some functions
+        }
         return JSON.stringify(a);
     });
 }
-;
-const parseRequest = (s) => {
-    const [firstLine, rest] = divideStringOn(s, '\r\n');
-    const [method, url, protocol] = firstLine.split(' ', 3);
-    const [headers, body] = divideStringOn(rest, '\r\n\r\n');
-    const parsedHeaders = headers.split('\r\n').reduce((map, header) => {
-        const [key, value] = divideStringOn(header, ': ');
-        return map.set(key, value);
-    }, new Map());
-    return { protocol, method, url, headers: parsedHeaders, body };
-};
-const divideStringOn = (s, search) => {
-    const index = s.indexOf(search);
-    const first = s.slice(0, index);
-    const rest = s.slice(index + search.length);
-    return [first, rest];
-};
-const compileResponse = (r) => `${r.protocol} ${r.statusCode} ${r.status}
-${Array.from(r.headers).map(kv => `${kv[0]}: 
-${kv[1]}`).join('\r\n')}
-${r.body}`;
 console.log(`server is listening on ${PORT}`);
 //# sourceMappingURL=app.js.map
--- dist/myscript.js
+++ dist/myscript.js
@@ -1,12 +1,12 @@
 function page_loading() {
     if (localStorage.getItem("in-system") == "true") {
-        //user is in system
-        var ul_nav = document.getElementById("ul-nav");
+        // user is in system
+        const ul_nav = document.getElementById("ul-nav");
         ul_nav.removeChild(document.getElementById("delete"));
-        var li_name = document.createElement("li");
+        const li_name = document.createElement("li");
         li_name.setAttribute("class", "nav-item dropdown");
         ul_nav.appendChild(li_name);
-        var a_name = document.createElement("a");
+        const a_name = document.createElement("a");
         a_name.setAttribute("class", "nav-link dropdown-toggle");
         a_name.setAttribute("id", "user_name");
         a_name.setAttribute("href", "#");
@@ -15,21 +15,21 @@ function page_loading() {
         a_name.setAttribute("role", "button");
         a_name.innerText = localStorage.getItem("name");
         li_name.appendChild(a_name);
-        var ul_menu = document.createElement("ul");
+        const ul_menu = document.createElement("ul");
         ul_menu.setAttribute("class", "dropdown-menu");
         li_name.appendChild(ul_menu);
-        var li1 = document.createElement("li");
-        var a1 = document.createElement("a");
+        const li1 = document.createElement("li");
+        const a1 = document.createElement("a");
         li1.appendChild(a1);
         a1.setAttribute("class", "dropdown-item");
         a1.setAttribute("href", "#");
         a1.innerText = "Настройки";
-        var li2 = document.createElement("li");
-        var hr = document.createElement("hr");
+        const li2 = document.createElement("li");
+        const hr = document.createElement("hr");
         li2.appendChild(hr);
         hr.setAttribute("class", "dropdown-divider");
-        var li3 = document.createElement("li");
-        var a3 = document.createElement("a");
+        const li3 = document.createElement("li");
+        const a3 = document.createElement("a");
         li3.appendChild(a3);
         a3.setAttribute("class", "dropdown-item");
         a3.setAttribute("href", "#");
@@ -37,9 +37,9 @@ function page_loading() {
         a3.innerText = "Выйти";
         ul_menu.appendChild(li1);
         if (localStorage.getItem("role") == "2") {
-            //it is admin
-            var li4 = document.createElement("li");
-            var a4 = document.createElement("a");
+            // it is admin
+            const li4 = document.createElement("li");
+            const a4 = document.createElement("a");
             li4.appendChild(a4);
             a4.setAttribute("class", "dropdown-item");
             a4.setAttribute("href", "#");
@@ -47,9 +47,9 @@ function page_loading() {
             ul_menu.appendChild(li4);
         }
         if (localStorage.getItem("role") == "1") {
-            //it is master
-            var li5 = document.createElement("li");
-            var a5 = document.createElement("a");
+            // it is master
+            const li5 = document.createElement("li");
+            const a5 = document.createElement("a");
             li5.appendChild(a5);
             a5.setAttribute("class", "dropdown-item");
             a5.setAttribute("href", "#");
@@ -60,7 +60,7 @@ function page_loading() {
         ul_menu.appendChild(li3);
     }
 }
-/*<li class="nav-item dropdown">
+/* <li class="nav-item dropdown">
       <a class="nav-link dropdown-toggle" me href="#" id="user-name" role="button"
         data-bs-toggle="dropdown" aria-expanded="false">
         Имя
@@ -72,113 +72,126 @@ function page_loading() {
       </ul>
     </li> */
 function set_name() {
-    document.getElementById("user-name").textContent = localStorage.getItem("name");
+    document.getElementById("user-name").textContent =
+        localStorage.getItem("name");
 }
 function logout() {
     localStorage.setItem("in-system", "false");
     if (window.location.pathname.includes("lk"))
         window.location.replace("/");
-    else
+    else {
         window.location.reload();
+    }
 }
 function check_autorization() {
     return localStorage.getItem("in-system") == "true";
 }
 function autorization(login = "", password = "") {
-    var xhr = new XMLHttpRequest();
-    //get data
+    const xhr = new XMLHttpRequest();
+    // get data
     if (login === "")
         login = document.getElementById("floatingInput").value;
     if (password === "")
-        password = document.getElementById("floatingPassword").value;
-    //send data
-    xhr.open('POST', '/info?login=' + login);
-    xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+        password = document.getElementById("floatingPassword")
+            .value;
+    // send data
+    xhr.open("POST", `/info?login=${login}`);
+    xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
     xhr.send(`{"login": "${login}", "password": "${password}"}`);
     xhr.onreadystatechange = func;
-    //recieve answer
+    // recieve answer
     function func() {
         if (xhr.readyState === 4) {
             if (xhr.status === 200) {
                 if (xhr.responseText != undefined && xhr.responseText != "") {
                     document.getElementById("pass_warning").textContent = "";
                     // send page
-                    let val = JSON.parse(xhr.responseText)[0];
+                    const val = JSON.parse(xhr.responseText)[0];
                     // let send_page = new XMLHttpRequest();
                     if (val.role > 1) {
-                        //admin
+                        // admin
                         // send_page.open("GET","/lk/admin");
                         window.location.replace("/lk/admin");
                     }
                     else if (val.role > 0) {
-                        //master
-                        //send_page.open("GET","/lk/master");
+                        // master
+                        // send_page.open("GET","/lk/master");
                         window.location.replace("/lk/master");
                     }
                     else {
-                        //client
+                        // client
                         // send_page.open("GET","/lk/client");
                         window.location.replace("/lk/client");
                     }
-                    //user is in system now
+                    // user is in system now
                     localStorage.setItem("in-system", "true");
                     localStorage.setItem("name", val.name);
                     localStorage.setItem("role", val.role);
                     // send_page.send();
                 }
                 else {
-                    //wrong login or password
-                    document.getElementById("pass_warning").textContent = "Неверный логин или пароль";
+                    // wrong login or password
+                    document.getElementById("pass_warning").textContent =
+                        "Неверный логин или пароль";
                 }
             }
             else {
-                document.getElementById("pass_warning").textContent = "Произошла ошибка на сервере";
+                document.getElementById("pass_warning").textContent =
+                    "Произошла ошибка на сервере";
             }
         }
     }
 }
 function registration() {
-    var xhr = new XMLHttpRequest();
-    var login = document.getElementById("sign-email").nodeValue;
-    var password = document.getElementById("sign-password").nodeValue;
-    var ok = true;
-    //checking correctness of login
-    if (login.match(/^[a-z][a-z0-9]*?([-_][a-z0-9]+){0,2}$/i) || login.match(/^\+7[0-9]{10}$/i)) {
-        //ok
-        document.getElementById("sign-email").className =
-            document.getElementById("sign-email").className.replace(/(?:^|\s)is_invalid(?!\S)/g, '');
-        document.getElementById("label_sign_email").nodeValue = "Email/Номер телефона";
+    const xhr = new XMLHttpRequest();
+    const login = document.getElementById("sign-email").nodeValue;
+    const password = document.getElementById("sign-password").nodeValue;
+    let ok = true;
+    // checking correctness of login
+    if (login.match(/^[a-z][a-z0-9]*?([-_][a-z0-9]+){0,2}$/i) ||
+        login.match(/^\+7[0-9]{10}$/i)) {
+        // ok
+        document.getElementById("sign-email").className = document
+            .getElementById("sign-email")
+            .className.replace(/(?:^|\s)is_invalid(?!\S)/g, "");
+        document.getElementById("label_sign_email").nodeValue =
+            "Email/Номер телефона";
         document.getElementById("pass_sign_warning").nodeValue = "";
     }
     else {
-        //not correct
+        // not correct
         document.getElementById("sign-email").className += "is_invalid";
-        document.getElementById("label_sign_email").nodeValue = "Некорректное значение";
-        document.getElementById("pass_sign_warning").nodeValue = "Пример: my_mail@yandex.ru \n +79991234567\n";
+        document.getElementById("label_sign_email").nodeValue =
+            "Некорректное значение";
+        document.getElementById("pass_sign_warning").nodeValue =
+            "Пример: my_mail@yandex.ru \n +79991234567\n";
         ok = false;
     }
-    //checking correctness of password
+    // checking correctness of password
     if (password.length >= 6) {
-        document.getElementById("sign-password").className =
-            document.getElementById("sign-password").className.replace(/(?:^|\s)is_invalid(?!\S)/g, '');
+        document.getElementById("sign-password").className = document
+            .getElementById("sign-password")
+            .className.replace(/(?:^|\s)is_invalid(?!\S)/g, "");
         document.getElementById("label_sign_password").nodeValue = "Пароль";
     }
     else {
         document.getElementById("sign-password").className += "is_invalid";
-        document.getElementById("label_sign_password").nodeValue = "Некорректное значение";
-        document.getElementById("pass_sign_warning").nodeValue += "Пароль должен содержать минимум 6 символов";
+        document.getElementById("label_sign_password").nodeValue =
+            "Некорректное значение";
+        document.getElementById("pass_sign_warning").nodeValue +=
+            "Пароль должен содержать минимум 6 символов";
         ok = false;
     }
-    //check if this user already exist
-    //send email
+    // check if this user already exist
+    // send email
     if (ok) {
-        //open window for entering code
-        //close modal window
+        // open window for entering code
+        // close modal window
     }
 }
 function registration_end() {
-    //add to DB
-    //autorization
+    // add to DB
+    // autorization
     // autorization(login, password);
 }
 //# sourceMappingURL=myscript.js.map
